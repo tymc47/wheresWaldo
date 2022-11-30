dfcf7d7b6b1ef63b83939eb3c229cb39fc73b97a
Terry Mak <tymc447@gmail.com>
2022-11-30 14:22:53 +0800

    add character icon and level thumbnail

---
+++ src/assets/CharacterIcon.ts
@@ -0,0 +1,14 @@
+import waldo from "./character/waldo.svg";
+import wilma from "./character/wilma.svg";
+import wizard from "./character/wizard.svg";
+import woof from "./character/woof.svg";
+import odlaw from "./character/odlaw.svg";
+import { characterName } from "../types";
+
+export const characterIcon: Record<characterName, string> = {
+  waldo,
+  wilma,
+  woof,
+  odlaw,
+  wizard,
+};
---
+++ src/assets/Thumbnail.ts
@@ -0,0 +1,14 @@
+import beach from "./thumbnail/beach_thumb.jpg";
+import snow from "./thumbnail/snow_thumb.jpg";
+import hollywood from "./thumbnail/hollywood_thumb.jpg";
+import space from "./thumbnail/space_thumb.jpg";
+import track from "./thumbnail/track_thumb.jpg";
+import { levelName } from "../types";
+
+export const thumbnail: Record<levelName, string> = {
+  beach,
+  snow,
+  hollywood,
+  space,
+  track,
+};
