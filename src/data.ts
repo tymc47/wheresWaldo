392a96dc3a83f39dffe2c6dbf0dff59853c26bfe
Terry Mak <tymc447@gmail.com>
2022-11-30 14:24:36 +0800

    add level data

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+import { LevelObj } from "./types";
+
+const levelEntries: LevelObj[] = [
+  {
+    name: "beach",
+    character: ["waldo", "wilma", "wizard", "woof", "odlaw"],
+  },
+  {
+    name: "space",
+    character: ["waldo", "wilma", "wizard", "woof", "odlaw"],
+  },
+  {
+    name: "snow",
+    character: ["waldo", "wilma", "wizard", "woof", "odlaw"],
+  },
+  {
+    name: "track",
+    character: ["waldo", "wilma", "wizard", "woof", "odlaw"],
+  },
+  {
+    name: "hollywood",
+    character: ["waldo", "wilma", "wizard", "woof", "odlaw"],
+  },
+];
+
+export default levelEntries;
