7abd20f85370606c35bc4504ae42e3e4ef0f6919
Terry Mak <tymc447@gmail.com>
2022-11-30 23:58:57 +0800

    add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Navbar from "./components/Navbar";
 import LevelList from "./components/LevelList";
+import { Routes, Route, useMatch } from "react-router-dom";
+import Playground from "./components/Playground";
+import { characterName, LevelObj } from "./types";
+import levelEntries from "./data";
 
 const theme = createTheme({
   palette: {
@@ -14,18 +18,66 @@ const theme = createTheme({
     secondary: {
       main: "#d8eefe",
       light: "#5f6c7b",
-      dark: "#094067",
+      dark: "#3da9fc",
+    },
+    info: {
+      main: "#ef3e37",
     },
   },
 });
 
 function App() {
+  const [gameStart, setGameStart] = useState<boolean>(false);
+  const [currentLevel, setCurrentLevel] = useState<LevelObj | null>(null);
+  const [characterStrike, setCharacterStrike] = useState<characterName[]>([]);
+  const levels = levelEntries;
+  const match = useMatch("/playground/:name");
+
+  const startGame = () => setGameStart(true);
+
+  const handleStrike = (name: characterName) => {
+    if (characterStrike.includes(name)) return;
+    const found = characterStrike.concat(name);
+    if (found) setCharacterStrike(found);
+    if (found && found.length === currentLevel?.character.length) {
+      setGameStart(false);
+    }
+  };
+
+  useEffect(() => {
+    if (match) {
+      const level = levels.find(
+        (entry: LevelObj) => entry.name === match.params.name
+      );
+      if (level) {
+        setCurrentLevel(level);
+      }
+    } else setCurrentLevel(null);
+  }, [match]);
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
         <CssBaseline />
-        <Navbar />
-        <LevelList />
+        <Navbar
+          currentLevel={currentLevel}
+          gameStart={gameStart}
+          startGame={startGame}
+          found={characterStrike}
+        />
+        <Routes>
+          <Route
+            path="/playground/:name"
+            element={
+              <Playground
+                level={currentLevel}
+                gameStart={gameStart}
+                handleStrike={handleStrike}
+              />
+            }
+          />
+          <Route path="/" element={<LevelList levels={levels} />} />
+        </Routes>
       </Box>
     </ThemeProvider>
   );
