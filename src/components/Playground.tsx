b15e3351564947facc3e291460170407edaf535c
Terry Mak <tymc447@gmail.com>
2022-12-02 14:10:22 +0800

    add identification of found characters

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,57 +1,66 @@
 import { Box } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { playgroundImg } from "../assets/playgroundImg";
-import { characterName, LevelObj } from "../types";
+import { CharacterLocation, coordinates, LevelObj } from "../types";
 
 interface PlaygroundProps {
   level: LevelObj | null;
   gameStart: boolean;
-  handleStrike: (name: characterName) => void;
+  handleGuess: (guess: coordinates) => void;
+  found: CharacterLocation[];
 }
 
-const Playground = ({ level, gameStart, handleStrike }: PlaygroundProps) => {
-  const playgroundRef = useRef<HTMLDivElement>(null);
-  const [playgroundX, setPlaygroundX] = useState<number | undefined>();
-  const [playgroundY, setPlaygroundY] = useState<number | undefined>();
+interface imageSize {
+  height: number;
+  width: number;
+}
 
-  const getPosition = () => {
-    const x = playgroundRef.current?.offsetLeft;
-    const y = playgroundRef.current?.offsetTop;
+const Playground = ({
+  level,
+  gameStart,
+  handleGuess,
+  found,
+}: PlaygroundProps) => {
+  const [windowChange, setWindowChange] = useState<boolean>(false);
+  const [playgroundXY, setPlaygroundXY] = useState<coordinates>({ X: 0, Y: 0 });
+  const [playgroundSize, setPlaygroundSize] = useState<imageSize>({
+    width: 0,
+    height: 0,
+  });
+  const playgroundRef = useCallback(
+    (node: HTMLImageElement) => {
+      if (node !== null) {
+        const { x, y, height, width } = node.getBoundingClientRect();
+        setPlaygroundXY({ X: x, Y: y });
+        setPlaygroundSize({
+          height,
+          width,
+        });
+      }
+    },
+    [windowChange, gameStart]
+  );
 
-    setPlaygroundX(x);
-    setPlaygroundY(y);
-  };
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowChange((prev) => !prev);
+    });
+    window.addEventListener("scroll", () => {
+      setWindowChange((prev) => !prev);
+    });
+  }, []);
 
-  const mockBackend = (
-    clickX: number,
-    clickY: number
-  ): characterName | undefined => {
-    if (clickX && clickY) return "wizard";
-
-    return undefined;
-  };
+  if (!level) return <div>404 Level Not Found</div>;
 
   const handleClick = (event: React.MouseEvent<HTMLImageElement>) => {
     if (!gameStart) return;
-    const [clickX, clickY] = [event.clientX, event.clientY];
-    const result = mockBackend(clickX, clickY);
-
-    if (result) {
-      handleStrike(result);
-    }
+    const guess: coordinates = {
+      X: (event.clientX - playgroundXY.X) / playgroundSize.width,
+      Y: (event.clientY - playgroundXY.Y) / playgroundSize.height,
+    };
+    console.log(guess);
+    handleGuess(guess);
   };
-
-  useEffect(() => {
-    getPosition();
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", getPosition);
-  }, []);
-
-  console.log(playgroundX, playgroundY);
-
-  if (!level) return <div>404 Level Not Found</div>;
 
   return (
     <Box
@@ -59,6 +68,7 @@ const Playground = ({ level, gameStart, handleStrike }: PlaygroundProps) => {
         display: "flex",
         justifyContent: "center",
         pt: "5vh",
+        filter: gameStart ? "" : "blur(3px)",
       }}
     >
       <Box
@@ -68,10 +78,36 @@ const Playground = ({ level, gameStart, handleStrike }: PlaygroundProps) => {
         sx={{
           width: "90%",
           boxShadow: 10,
-          filter: gameStart ? "" : "",
+          cursor: "crosshair",
+          position: "relative",
         }}
         onClick={handleClick}
       />
+      {found.length === 0
+        ? null
+        : found.map((location) => (
+            <Box
+              key={location.name}
+              sx={{
+                border: "3px solid #ef3e37",
+                width: `${playgroundSize.height * 0.02}px`,
+                height: `${playgroundSize.height * 0.02}px`,
+                position: "absolute",
+                top: `${
+                  playgroundXY.Y +
+                  window.scrollY +
+                  location.coordinates.Y * playgroundSize.height -
+                  (playgroundSize.height * 0.02) / 2
+                }px`,
+                left: `${
+                  playgroundXY.X +
+                  window.scrollX +
+                  location.coordinates.X * playgroundSize.width -
+                  (playgroundSize.height * 0.02) / 2
+                }px`,
+              }}
+            />
+          ))}
     </Box>
   );
 };
